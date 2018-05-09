1b485482b6116a8d9db92c11f865be64c35dbe35
zsg <zhoushugangnuliba@163.com>
2018-05-09 22:24:56 +0800

    添加了两根斜线

--- app/src/main/java/com/example/zsg/broadcastbesttest/LoginActivity.java
+++ app/src/main/java/com/example/zsg/broadcastbesttest/LoginActivity.java
@@ -31,6 +31,7 @@ public class LoginActivity extends BaseActivity {
                     finish();
                     //添加了dev分支
                     //添加了注释
+                    //
                 } else {
                     Toast.makeText(LoginActivity.this, "account or password is invalid", Toast.LENGTH_SHORT).show();
                 }
